9294b50931ecacaa9d04b7b42fe461f4aa91f365
Vu Van Huy STC <huyvv@vnpt-technology.vn>
2022-04-15 15:12:02 +0700

    change config

--- config-client/src/main/java/com/profesorp/configclient/ConfigServiceController.java
+++ config-client/src/main/java/com/profesorp/configclient/ConfigServiceController.java
@@ -9,36 +9,60 @@ import com.profesorp.configclient.bean.BeanConfiguration;
 
 @RestController
 public class ConfigServiceController {
-	
-	@Value("${valores.valor_fijo}")
-	String valorFijo;
-	
-	@Value("${valores.valor_funcion}") 
-	String valorFuncion;
-	
-	@Autowired
-	private Configuration configuration;
 
-	@Autowired
-	private ConfigurationData configurationData;
+    @Value("${valores.valor_fijo}")
+    String valorFijo;
 
-	@GetMapping("/configuration-bean")
-	public BeanConfiguration getConfiguracion()
-	{
-		return new BeanConfiguration(configuration.getMinimum(),
-				configuration.getMaximum(),valorFijo,valorFuncion);
-	}
-	
-	
-	@GetMapping("/configuration-value")
-	public BeanConfiguration getConfiguracionRefrescada(@Value("${valores.valor_funcion}") String valorFuncion)
-	{
-		return new BeanConfiguration(configuration.getMinimum(),
-				configuration.getMaximum(),valorFijo,valorFuncion);
-	}
-	@GetMapping("/configuration-properties")
-	public ConfigurationData retrieveDatosFromConfigurations() {
-		return configurationData;
-	}
-	
+    @Value("${valores.valor_funcion}")
+    String valorFuncion;
+
+    @Autowired
+    private Configuration configuration;
+
+    @Autowired
+    private ConfigurationData configurationData;
+
+    @Value("${datosservidor.minimum}")
+    Integer minimum;
+
+    @Value("${datosservidor.maximum}")
+    Integer maximum;
+
+    @GetMapping("/configuration-bean")
+    public BeanConfiguration getConfiguracion() {
+        return new BeanConfiguration(configuration.getMinimum(),
+            configuration.getMaximum(), valorFijo, valorFuncion);
+    }
+
+
+    @GetMapping("/configuration-value")
+    public BeanConfiguration getConfiguracionRefrescada(@Value("${valores.valor_funcion}") String valorFuncion) {
+        return new BeanConfiguration(configuration.getMinimum(),
+            configuration.getMaximum(), valorFijo, valorFuncion);
+    }
+
+    @GetMapping("configuration-by-props")
+    public ConfigurationData getConfiguracionProp() {
+        ConfigurationData cfg = new ConfigurationData();
+        cfg.setMinimum(minimum);
+        cfg.setMaximum(maximum);
+        return cfg;
+    }
+
+    @GetMapping("/configuration-by-value")
+    public ConfigurationData getConfiguracionRefrescada(
+        @Value("${datosservidor.minimum}") Integer minimum,
+        @Value("${datosservidor.maximum}") Integer maximum
+    ) {
+        ConfigurationData cfg = new ConfigurationData();
+        cfg.setMinimum(minimum);
+        cfg.setMaximum(maximum);
+        return cfg;
+    }
+
+    @GetMapping("/configuration-by-config-bean")
+    public ConfigurationData retrieveDatosFromConfigurations() {
+        return configurationData;
+    }
+
 }
